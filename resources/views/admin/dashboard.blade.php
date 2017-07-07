dafe7e49b5eb04991e2f0fd050fdf3599f0e7a6a
Hendri <hendri.gnw@gmail.com>
2017-07-07 16:19:30 +0700

    update view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 @section('title', 'Dashboard')
+@section('breadcrumbs')
+	@php echo \Breadcrumbs::render(['Dashboard']) @endphp
+@endsection
 @section('content')
 	<div class="panel panel-default">
 		<div class="panel-heading">Dashboard</div>
