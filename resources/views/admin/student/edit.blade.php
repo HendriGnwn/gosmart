5786a385575e54026798e47e4c2728efdbc78344
Hendri <hendri.gnw@gmail.com>
2017-07-13 16:35:14 +0700

    update teacher crud

--- resources/views/admin/student/edit.blade.php
+++ resources/views/admin/student/edit.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app.frame')
-@section('title', 'Edit User')
+@section('title', 'Edit Student')
 @section('description', 'Please make sure to check all input')
 @section('breadcrumbs')
-	@php echo \Breadcrumbs::render([['title' => 'User', 'url' => url('/admin/user')], 'Update']) @endphp
+	@php echo \Breadcrumbs::render([['title' => 'Student', 'url' => url('/admin/student')], 'Update']) @endphp
 @endsection
 @section('button')
-	<a href="{{ url('/admin/user') }}" class="btn btn-info btn-xs no-border">Back</a>
+	<a href="{{ url('/admin/student') }}" class="btn btn-info btn-xs no-border">Back</a>
 @endsection
 
 @section('content')
@@ -13,12 +13,12 @@
 
     {!! Form::model($model, [
             'method' => 'PATCH',
-            'url' => ['/admin/user', $model->id],
+            'url' => ['/admin/student', $model->id],
             'files' => true,
             'id' => 'formValidate',
         ]) !!}
 
-        @include ('admin.user.form', ['submitButtonText' => 'Update'])
+        @include ('admin.student.form', ['submitButtonText' => 'Update'])
 
 	{!! Form::close() !!}
 @endsection
